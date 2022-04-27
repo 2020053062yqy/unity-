0e5ebdf6fee0d655cd34d12f7808341e42f1d209
2020053062yqy <104482693+2020053062yqy@users.noreply.github.com>
2022-04-27 19:53:41 +0800

    Update NewBehaviourScript.cs

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-     public float MoveForce = 100.0f;
-    public float MaxSpeed = 5;
-    public Rigidbody2D HeroBody;
+     public float MoveForce = 100.0f; //角色移动力度
+    public float MaxSpeed = 5;    //初始水平速度为5
+    public Rigidbody2D HeroBody;  
     [HideInInspector]
-    public bool bFaceRight = true;
+    public bool bFaceRight = true;  
     [HideInInspector]
-    public bool bJump = false;
-    public float JumpForce = 100;
-    private Transform mGroundCheck;
+    public bool bJump = false;    //角色初始状态是禁止的
+    public float JumpForce = 100;//角色跳跃力度
+    private Transform mGroundCheck;  //查看是否为地面
     // Start is called before the first frame update
     void Start()
     {
-        HeroBody = GetComponent<Rigidbody2D>();
-        mGroundCheck = transform.Find("GroundCheck");
+        HeroBody = GetComponent<Rigidbody2D>(); //引用组件Rigidbody2D
+        mGroundCheck = transform.Find("GroundCheck"); //是否找到地面
     }
 
     // Update is called once per frame
     void Update()
     {
-         float h = Input.GetAxis("Horizontal");
+         float h = Input.GetAxis("Horizontal");    //获取水平轴
         if(Mathf.Abs(HeroBody.velocity.x)<MaxSpeed)
         {
             HeroBody.AddForce(Vector2.right * h * MoveForce);
